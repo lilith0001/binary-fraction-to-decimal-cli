e651f4a8d25f71712b4b810568220131dd12b53a
lilith <90871823+lilith0001@users.noreply.github.com>
2022-03-02 12:22:26 -0400

    Add files via upload

---
+++ binconvert.py
@@ -0,0 +1,38 @@
+# handling input
+binary = input("enter the binary string: ")
+binary = binary.replace(" ","")
+binsplit = binary.split('.')
+valid = False
+while not valid:
+	if len(binsplit) < 3:
+		flag = True
+		for split in binsplit:
+			if not flag:
+				break
+			for char in split:
+				if char != '0' and char != '1':
+					flag = False
+					break
+		if flag:
+			valid = True
+			break
+	binary = input("invalid input. try again: ")
+	binary = binary.replace(" ","")
+	binsplit = binary.split('.')
+
+# decimal conversion 
+decimal = 0
+power = 0
+for digit in reversed(binsplit[0]):
+	if digit != ' ':
+		if int(digit) == 1:
+			decimal += 2**power
+		power += 1
+if len(binsplit) > 1:
+	power = -11
+	for digit in binsplit[1]:
+		if digit != ' ':
+			if int(digit) == 1:
+				decimal += 2**power
+			power -= 1
+print(f"decimal conversion: {decimal}")
